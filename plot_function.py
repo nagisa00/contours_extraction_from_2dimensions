6ec651364b9c2d4059f4b06b50db1e547bd7aa94
Nagisa Matsumoto <nkysc.1929.r@keio.jp>
2019-04-27 11:39:34 +0900

    散布図、輪郭線をプロットするためのファイル

---
+++ plot_function.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+
+def scatter_plot(x,y,label_name):
+    plt.scatter(x,y,label=label_name)
+    print('scatter_plot')
+
+
+def contours_plot(contours, label_name):
+    x_plot = [raw[0] for raw in contours]
+    y_plot = [raw[1] for raw in contours]
+    x_first_last = [x_plot[0], x_plot[-1]]
+    y_first_last = [y_plot[0], y_plot[-1]]
+
+    plt.plot(x_plot,y_plot, label=label_name)
+    plt.plot(x_first_last, y_first_last, label=label_name)
+
+    print('contours_plot')
+
+def show_image():
+    plt.show()
+
+
+def change_setting():
+    print('change_setting')
