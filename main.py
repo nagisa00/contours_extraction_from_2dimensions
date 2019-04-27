ddcef130381f5379ccf25d3ae9d940db8258613d
Nagisa Matsumoto <nkysc.1929.r@keio.jp>
2019-04-27 14:49:19 +0900

    データセットに応じて境界の色を変更

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from monotone_chain import get_convex_hull
 from collect_data import get_data
+from collect_data import get_several_data_as_csv
+from plot_function import setting_axis
 from plot_function import scatter_plot
 from plot_function import contours_plot
 from plot_function import show_image
-import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    data_sheet_name = input('Please input data sheet name')
-    x, y, data = get_data(data_sheet_name)
-    contours = get_convex_hull(data)
-    print(contours)
-    
-    scatter_plot(x,y,'raw_data')
-    contours_plot(contours, 'contours_plot')
+    file_path = input("Please input path to files")
+    path = file_path + '/*.csv'
+    data_set, data_set_num = get_several_data_as_csv(path)
+    #x, y, data = get_data(data_sheet_name)
+    contours = [get_convex_hull(data) for data in data_set]
+    #print(contours)
+    #setting_axis(left_x, rigth_x, up_y, down_y)
+    #scatter_plot(x,y,'raw_data')
+    contours_plot(contours[0], 'contours_plot', 'red', 3.0)
+    contours_plot(contours[1], 'contours_plot2', 'blue', 3.0)
     show_image()
 
--- plot_function.py
+++ plot_function.py
@@ -1,16 +1,26 @@
 import matplotlib.pyplot as plt
 
+dict_color = {0:'red', 1:'blue', 2:'green'}
+
+def setting_axis(left, right, up, down):
+    # 軸の設定
+    plt.figure()
+    plt.axis([left, right, up, down])
+    #ax = plt.subplot()
+    #ax.grid(which="both")
+
+
 def scatter_plot(x,y,label_name):
     plt.scatter(x,y,label=label_name)
     print('scatter_plot')
 
 
-def contours_plot(contours, label_name, color_name, line_width):
+def contours_plot(contours, label_name, color_code, line_width):
     x_plot = [raw[0] for raw in contours]
     y_plot = [raw[1] for raw in contours]
     x_first_last = [x_plot[0], x_plot[-1]]
     y_first_last = [y_plot[0], y_plot[-1]]
-
+    color_name = dict_color[color_code]
     plt.plot(x_plot,y_plot, label=label_name, color=color_name, linewidth=line_width)
     plt.plot(x_first_last, y_first_last, label=label_name, color = color_name, linewidth=line_width)
 
