7db439143f7cd3a7f024632415d867b6bd0b1e88
Nagisa Matsumoto <nkysc.1929.r@keio.jp>
2019-04-27 11:49:50 +0900

    領域描画に色と線幅の指定を追加

--- plot_function.py
+++ plot_function.py
@@ -5,14 +5,14 @@ def scatter_plot(x,y,label_name):
     print('scatter_plot')
 
 
-def contours_plot(contours, label_name):
+def contours_plot(contours, label_name, color_name, line_width):
     x_plot = [raw[0] for raw in contours]
     y_plot = [raw[1] for raw in contours]
     x_first_last = [x_plot[0], x_plot[-1]]
     y_first_last = [y_plot[0], y_plot[-1]]
 
-    plt.plot(x_plot,y_plot, label=label_name)
-    plt.plot(x_first_last, y_first_last, label=label_name)
+    plt.plot(x_plot,y_plot, label=label_name, color=color_name, linewidth=line_width)
+    plt.plot(x_first_last, y_first_last, label=label_name, color = color_name, linewidth=line_width)
 
     print('contours_plot')
 
