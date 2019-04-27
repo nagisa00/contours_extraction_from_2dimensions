11405dd1d1725abd7e1451516a03675378dbfef9
Nagisa Matsumoto <nkysc.1929.r@keio.jp>
2019-04-27 11:40:20 +0900

    プロット部分をplot_functionに切り離した

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from monotone_chain import get_convex_hull
 from collect_data import get_data
+from plot_function import scatter_plot
+from plot_function import contours_plot
+from plot_function import show_image
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     data_sheet_name = input('Please input data sheet name')
     x, y, data = get_data(data_sheet_name)
     contours = get_convex_hull(data)
     print(contours)
+    
+    scatter_plot(x,y,'raw_data')
+    contours_plot(contours, 'contours_plot')
+    show_image()
 
